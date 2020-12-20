884ffd70d25c55642d2e61625b5eb4cb0b5342d3
Location-Artistry <erickerney@gmail.com>
2020-12-20 10:17:10 -0500

    added PAleafMap.html

--- functions/index.js
+++ functions/index.js
@@ -12,11 +12,12 @@ const firebaseConfig = {
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(firebaseConfig);
+const puppeteer = require('puppeteer');
 const fetch = require('node-fetch');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors({ origin: true }))
+app.use(cors({ origin: true }));
 
 app.get("/purpleAirData/:stationList", async (req, res) => {
   const sta_list = req.params.stationList.split(',');  
@@ -65,6 +66,7 @@ app.get("/purpleAirData/:stationList", async (req, res) => {
       stationsObject.features[x] = stationAQI;
     }
     res
+    .set('Access-Control-Allow-Origin', '*')
     .status(200)
     .send(stationsObject);
     }
@@ -72,8 +74,108 @@ app.get("/purpleAirData/:stationList", async (req, res) => {
     console.error("Your AQ Fetch is gone all WRONG");
     res
     .status(400)
-    .send('ERROR MESSAGE bad GeoJSON mate!');
+    .send('Your AQ Fetch is gone all WRONG');
     }
 });
                                
+app.get('/msuWeatherData/:stations', async (request, response) => {
+      const browser1 = await puppeteer.launch({headless: true, devtools: false, 
+        args: ['--no-sandbox', '--user-data-dir=./chromeData', '--disable-setuid-sandbox']});
+      const browser2 = await puppeteer.launch({headless: true, devtools: false,
+        args: ['--no-sandbox', '--user-data-dir=./chromeData', '--disable-setuid-sandbox']});
+      const page1 = await browser1.newPage();
+      const page2 = await browser2.newPage();
+      
+  try {
+      const sta_list = request.params.stations.split(',');  
+      let scrapeRecord = await stationScrape(sta_list);
+      browser1.close(); 
+      browser2.close();
+      response
+      .set('Access-Control-Allow-Origin', '*')
+      .status(200)
+      .json(scrapeRecord);   
+  } catch (error) {
+      console.log('ERROR!!!!');
+  }  
+//abbreviations for weather station locations
+//meta function - loops through list of station IDs
+async function stationScrape(stationList) {
+  console.log(stationList);
+  //let stationList = ['men''cld', 'cer', 'alb', 'cnt', 'kzo'];
+  
+  const urlData = 'https://enviroweather.msu.edu/weather.php?stn=';
+  const urlCoords = 'https://mawn.geo.msu.edu/station.asp?id=';
+  var scrapeRun = [];
+  var scrapeStats = {};
+  try {
+      let x = 0;
+      for(staID of stationList) {
+          scrapeStats = await scrapeSite((urlData + staID), (urlCoords + staID), staID);
+          console.log("StationID:" + staID); 
+          scrapeRun[x] = scrapeStats;
+          x++;
+      }         
+  }
+  catch {
+      console.log("error return");
+  }
+  return (scrapeRun);
+}       
+//this is primary scraper function, url is passed with staID added to end of address
+async function scrapeSite(url1, url2, id) {
+  //station data page access to scrape
+  console.log("Scraping:" + id);
+  await page1.goto(url1);
+  //geolocation coordinates are on a separate page, also load that page :()
+  await page2.goto(url2);
+  let [el] = "", txt, valTxt, [el2] = "", txt2, labelTxt, [el3] = "", txt3, titleTxt, testTxt,
+  scrapeError = 'FALSE', keyIndex = 0, [el4] ="", txt4, lonLat, trmTxt;
+  //Title of station - outside of parameters loop
+  [el3] = await page1.$x('//*[@id="weatherPadding"]/h1');
+  txt3 = await el3.getProperty('textContent');
+  titleTxt = await txt3.jsonValue();
+  var jsonObject = { type:"Feature", properties: {stationID: staID, stationName: titleTxt }};
+  let keyList = ['airTemp', 'rainfallToday', 'relHumidity', 'dewpoint', 'windDir', 'windSpeed', 'leafWetness'];     
+  //Loop through all parameters shown on station webpage, stop when next value is undefined
+  for (i=2; scrapeError!='TRUE';i++) {
+      try {
+          [el] = await page1.$x('//*[@id="latestObsWeather"]/table/tbody/tr[' + i +']/td');
+          txt = await el.getProperty('textContent');
+          trmTxt = await txt.jsonValue(), valTxt = trmTxt.trim();
+          [el2] = await page1.$x('//*[@id="latestObsWeather"]/table/tbody/tr[' + i + ']/th');
+          txt2 = await el2.getProperty('textContent');
+          labelTxt = await txt2.jsonValue();
+      } catch (error) {
+          console.log('ERROR eh?');
+          scrapeError = 'TRUE';
+      }
+      try {
+          //test of next set of text values to see if at end of list
+          [eltest] = await page1.$x('//*[@id="latestObsWeather"]/table/tbody/tr[' + (i+1) +']/td');
+          txtest = await eltest.getProperty('textContent');
+          testTxt = await txtest.jsonValue();
+      } catch (error) {
+          console.log('End of parameters');
+          scrapeError = 'TRUE';
+      }  
+  jsonObject.properties[keyList[keyIndex]] = valTxt;
+  keyIndex++
+  }
+  jsonObject.geometry = {type:"Point", coordinates: [-1, -1]};
+  //scrape coordinates from url2
+  for (x=0;x<2;x++) {
+      [el4] = await page2.$x('/html/body/table/tbody/tr[2]/td/table[1]/tbody/tr/td[1]/table/tbody/tr[' + (x+7) + ']/td[2]');
+      txt4 = await el4.getProperty('textContent');
+      let coordsTemp = await txt4.jsonValue();
+      lonLat = coordsTemp.substring(0, 8);
+      jsonObject.geometry.coordinates[1-x] = lonLat;
+  }
+  console.log("Finished Scraping:" + id);
+  return jsonObject;
+}
+//browser1.close(); 
+//browser2.close();
+}); 
+
 exports.api = functions.runWith({ memory: '2GB' }).https.onRequest(app);
