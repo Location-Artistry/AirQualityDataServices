75c9e5f05312c4350e5abefe04c3a1daddf40de5
Location-Artistry <erickerney@gmail/com>
2021-09-10 14:53:06 -0400

    improved functions with time + remove null

--- functions/index.js
+++ functions/index.js
@@ -148,20 +148,33 @@ app.get("/purpleAirData/:stationList", async (req, res) => {
   const sta_list = req.params.stationList.split(',');  
   const metaData = "GeoJSON of Purple Air Stations requested by station ID, data from PA API, AQI Index calculated using USEPA methods by NHBP, WGS 84 Coordinate System";
   let stationsObject = { "type": "FeatureCollection", "metadata": metaData, "features": []}, airObject = {}; 
+  
+  convertMS = (milliseconds) => {
+    let seconds = Math.floor(milliseconds / 1000);
+    let minute = Math.floor(seconds / 60);
+    let hour = Math.floor(minute / 60);
+    let day = Math.floor(hour / 24);
+    return { day: day, hour: (hour % 24), minute: (minute % 60), seconds: (seconds % 60)};
+  };
+
   const airQualityFetch = async (staID) => {
     const airData = await fetch(`https://www.purpleair.com/json?show=${staID}`); 
     const airDataJSON = await airData.json();  
-    const {Label, ID, PM2_5Value, Lat, Lon, Flag, Version, RSSI} = airDataJSON.results[0], sta_stats = (airDataJSON.results[0].Stats).split(":");
-    //console.log(sta_stats[7].substr(0, 4));
-    const pm25_1hour = Number((sta_stats[4]).substr(0, 4)), pm25_24hour = Number((sta_stats[6]).substr(0, 4)), pm25_1week = Number((sta_stats[7]).substr(0, 4));
+    const {Label, ID, PM2_5Value, Lat, Lon, Flag, Version, RSSI, LastSeen} = airDataJSON.results[0], sta_stats = (airDataJSON.results[0].Stats).split(/[:,"]/);
+    const pm25_1hour = Number(sta_stats[15]), pm25_24hour = Number(sta_stats[23]), pm25_1week = Number(sta_stats[27]);
     //console.log(`${Label} ${ID} ${PM2_5Value} ${Lat} ${Lon} ${pm25_1hour} ${pm25_24hour}`);
     const AQI = await airQualityIndex(PM2_5Value), AQI1Hour = await airQualityIndex(pm25_1hour);
     const AQI24Hour = await airQualityIndex(pm25_24hour), AQI1Week = await airQualityIndex(pm25_1week);
     const diff_val = Math.abs(airDataJSON.results[0].PM2_5Value - airDataJSON.results[1].PM2_5Value).toFixed(2);
-    //console.log(`${(await currentAQI).aqi_val} ${(await currentAQI).aqi} ${(await oneHourAQI).aqi_val}`);
+    const lastSeenUTC = LastSeen + '000', nowTime = new Date();
+    const lastSeenTime = new Date(Number(lastSeenUTC));
+    const timeSinceSeen = ((nowTime.getTime()) - (lastSeenTime.getTime()));
+    const t = convertMS(timeSinceSeen);
+    
     airObject = { 'type': 'Feature', 'properties': {'ID':ID,'label':Label,'flagged_high':Flag,'version':Version,'WiFiSignal':RSSI,
-                'PM2_5Value':PM2_5Value,'PM2_5_1hour':pm25_1hour,'PM2_5_24hour':pm25_24hour,'PM2_5_1week':pm25_1week,'sensorDiff':diff_val,'AQI':AQI.aqi_val,'AQIText':AQI.aqi,'AQI1Hour':AQI1Hour.aqi_val,'AQI1HourText':AQI1Hour.aqi,
-                'AQI24Hour':AQI24Hour.aqi_val,'AQI24HourText':AQI24Hour.aqi,'AQI1Week':AQI1Week.aqi_val,'AQI1WeekText':AQI1Week.aqi},
+                'PM2_5Value':Number(PM2_5Value),'PM2_5_1hour':pm25_1hour,'PM2_5_24hour':pm25_24hour,'PM2_5_1week':pm25_1week,'sensorDiff':diff_val,'AQI':AQI.aqi_val,'AQIText':AQI.aqi,'AQI1Hour':AQI1Hour.aqi_val,'AQI1HourText':AQI1Hour.aqi,
+                'AQI24Hour':AQI24Hour.aqi_val,'AQI24HourText':AQI24Hour.aqi,'AQI1Week':AQI1Week.aqi_val,'AQI1WeekText':AQI1Week.aqi, 'lastSeen': lastSeenTime,
+                'lastSeenUTC': lastSeenTime.getTime(),'dayHourMinSecSinceSeen': (`${t.day}:${t.hour}:${t.minute}:${t.seconds}`), 'formatSinceSeen': t},
                 'geometry': { 'type':'Point','coordinates': [Lon,Lat] } };
     return airObject;   
   };
@@ -173,11 +186,9 @@ app.get("/purpleAirData/:stationList", async (req, res) => {
     const aqi_range = ["Good","Moderate","Unhealthy for Sensitive Groups","Unhealthy","Very Unhealthy","Hazardous","Hazardous"];
     for (z in aqi_range) {
       if (airValue > c_high[z]) {
-      //console.log('Status = Current');
       } else { aqi_val = (aqi_fac[z] * (airValue - c_low[z]) + aqi_low[z]); 
           aqi_val = Math.round(aqi_val); 
           aqi = aqi_range[z];
-          //console.log(`test function ${airValue}, ${aqi_val}, ${aqi}`);   
           return {aqi_val, aqi};
           }
       }
@@ -187,7 +198,6 @@ app.get("/purpleAirData/:stationList", async (req, res) => {
     
     for(x in sta_list){
       const stationAQI = await airQualityFetch(sta_list[x]);
-      //console.log(await stationAQI);
       stationsObject.features[x] = stationAQI;
     }
     res
